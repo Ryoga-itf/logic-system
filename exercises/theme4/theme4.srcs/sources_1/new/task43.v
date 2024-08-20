078765b458b9a862150cfe73eb8ae4452badbf80
Ryoga-exe <git@ryoga.dev>
2024-08-21 01:59:55 +0900

    feat: impl theme4_3_2

--- exercises/theme4/theme4.srcs/sources_1/new/task43.v
+++ exercises/theme4/theme4.srcs/sources_1/new/task43.v
@@ -3,7 +3,7 @@
 
 `timescale 1ns / 1ps
 
-module task43(clk, sw, push, pop, add, ld, ld_full, ld_empty);
+module task43(clk, sw, push, pop, add, push_result, ld, ld_full, ld_empty);
     `include "parameters_task43.v"
 
     input clk;
@@ -11,23 +11,26 @@ module task43(clk, sw, push, pop, add, ld, ld_full, ld_empty);
     input push;
     input pop;
     input add;
-    output reg [IO_WIDTH-1:0] ld;
+    input push_result;
+    output [IO_WIDTH-1:0] ld;
     output ld_full;
     output ld_empty;
 
-    wire [2:0] pulse_btn;
+    wire [3:0] pulse_btn;
 
     wire [IO_WIDTH-1:0] value;
-    reg [2:0]pop_data_timer;
+    reg [2:0]pop_w;
+    reg push_result_w;
 
     pulse_generator _btn0(.clk(clk), ._in(push), ._out(pulse_btn[0]));
     pulse_generator _btn1(.clk(clk), ._in(pop), ._out(pulse_btn[1]));
     pulse_generator _btn2(.clk(clk), ._in(add), ._out(pulse_btn[2]));
+    pulse_generator _btn3(.clk(clk), ._in(push_result), ._out(pulse_btn[3]));
 
 
     stack_ctrl s0(.clk(clk),
-        .din(sw),
-        ._push(pulse_btn[0]),
+        .din(push_result_w ? result : sw),
+        ._push(pulse_btn[0] || push_result_w),
         ._pop(pulse_btn[1]),
         .dout(value),
         ._full(ld_full),
@@ -37,29 +40,43 @@ module task43(clk, sw, push, pop, add, ld, ld_full, ld_empty);
 
     localparam N = 2;
     reg [IO_WIDTH-1:0] opr[0:N-1];
+    reg [IO_WIDTH-1:0] result;
     
     initial begin
         opr[0] <= 0;
         opr[1] <= 0;
-        pop_data_timer <= 0;
+        pop_w <= 0;
+        push_result_w <= 0;
+        result <= 0;
     end
 
     always@(posedge clk) begin
-        if (pop_data_timer == 0) begin
+        if (push_result_w) begin
+            push_result_w <= 0;
+        end
+
+        if (pop_w == 0) begin
             if (pulse_btn[1]) begin
-                pop_data_timer <= 1;
+                pop_w <= 1;
             end
         end else begin
-            pop_data_timer <= pop_data_timer + 1;
-            if (pop_data_timer == 2) begin
+            pop_w <= pop_w + 1;
+            if (pop_w == 2) begin
                 opr[0] <= value;
                 opr[1] <= opr[0];
-                pop_data_timer <= 0;
+                pop_w <= 0;
             end
         end
 
         if (pulse_btn[2]) begin
-            ld <= opr[1] + opr[0];
+            result <= opr[1] + opr[0];
+        end
+
+        if (pulse_btn[3]) begin
+            push_result_w <= 1;
         end
     end
+
+    assign ld = result;
+
 endmodule // task43

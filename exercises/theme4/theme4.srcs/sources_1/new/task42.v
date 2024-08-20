291b3703e66dd1caa339a84bf7419f4594d57db6
Ryoga-exe <git@ryoga.dev>
2024-08-20 15:41:20 +0900

    feat: add theme4_2

---
+++ exercises/theme4/theme4.srcs/sources_1/new/task42.v
@@ -0,0 +1,76 @@
+// File: task42.v
+// Author: Ryoga <@Ryoga-exe>
+
+`timescale 1ns / 1ps
+
+module pulse_generator(clk, _in, _out);
+    localparam SAMPLING_PERIOD = 1250000-1; // for implementation
+    // localparam SAMPLING_PERIOD = 10; // for simulation
+
+    input clk;
+    input _in;
+    output _out;
+
+    reg [1:0]r;
+    reg [31:0]cnt;
+
+    always@(posedge clk) begin
+        if (cnt < SAMPLING_PERIOD) cnt <= cnt + 1;
+        else cnt <= 0;
+        if (cnt == SAMPLING_PERIOD)r[0] <= _in;
+        r[1] <= r[0];
+    end
+
+    assign _out = r[1] & ~r[0];
+endmodule // pulse_generator
+
+module task42(clk, sw, btn, ld);
+    localparam N = 2;
+
+    localparam S_WAIT  = 2'b00;
+    localparam S_READY = 2'b01;
+    localparam S_ADD   = 2'b10;
+
+    input clk;
+    input [3:0] sw;
+    input [1:0] btn;
+    output reg [3:0] ld;
+
+    reg [1:0] state;
+    reg [3:0] opr[0:N-1];
+    reg [1:0] opr_cnt;
+    wire[1:0] pulse_btn;
+
+    pulse_generator _btn0(.clk(clk), ._in(btn[0]), ._out(pulse_btn[0]));
+    pulse_generator _btn1(.clk(clk), ._in(btn[1]), ._out(pulse_btn[1]));
+
+    initial begin
+        state <= S_WAIT;
+        opr[0] <= 0;
+        opr[1] <= 0;
+        opr_cnt <= 0;
+    end
+
+    always@(posedge clk) begin
+        if (state == S_WAIT) begin
+            if (pulse_btn[0]) begin
+                opr[0] <= sw;
+                opr[1] <= opr[0];
+                opr_cnt <= opr_cnt + 1;
+            end
+            if (opr_cnt == 2) begin
+                state <= S_READY;
+            end
+        end else if (state == S_READY) begin
+            if (pulse_btn[1]) begin
+                state = S_ADD;
+            end
+        end else if (state == S_ADD) begin
+            ld <= opr[0] + opr[1];
+            opr[0] <= 0;
+            opr[1] <= 0;
+            opr_cnt <= 0;
+            state = S_WAIT;
+        end
+    end
+endmodule // task42
